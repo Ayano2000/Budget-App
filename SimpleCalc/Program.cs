2f490f0895d4743ebf07d2869d4a94a5c319b405
Sakhile Msibi <sakhilema@bbd.co.za>
2020-02-17 10:13:06 +0200

    This is a basic budget app

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    public class Program
+     {
+         static void Main(string[] args)
+        {
+            BudgetClass budgetClass = new BudgetClass();
+            budgetClass.Budget();
+        }
+    }
+}
