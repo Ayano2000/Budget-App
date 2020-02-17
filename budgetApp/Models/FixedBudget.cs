880ab9ae77564321b495471c834fce1452e0967a
Sakhile Msibi <sakhilema@bbd.co.za>
2020-02-17 13:33:36 +0200

    Bud

--- budgetApp/Models/FixedBudget.cs
+++ budgetApp/Models/FixedBudget.cs
@@ -17,12 +17,18 @@ namespace budgetApp.Models {
         public int Salary { get; set; }
         public int Rent { get; set; }
         public int Insurance { get; set; }
-        public int MedicalAid { get; set; } // likelyhood to rise.
+        public int MedicalAid { get; set; }
+
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public int Priority { get; set; }
+        public int Rise { get; set; }
 
 
      List<FixedBudget> fixedbudget = new List<FixedBudget>
      {
-         new FixedBudget { Salary = 0, Rent = 0, Insurance= 0, MedicalAid=0}
+         new FixedBudget { Salary = 0, Rent = 0, Insurance= 0, MedicalAid=0},
+         new FixedBudget{Name = "", Amount = 0, Priority = 0, Rise = 0}
      };
 
     }
