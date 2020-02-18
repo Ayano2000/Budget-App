0aefddf07f21906295095718337734f38b7a27ed
hbarnardWTC <wingarkorin@gmail.com>
2020-02-18 08:22:02 +0200

    Testing Merge

--- budgetApp/Program.cs
+++ budgetApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using budgetApp.Models;
 
 namespace budgetApp
 {
@@ -14,6 +15,13 @@ namespace budgetApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            List<Item> items =InOut.readBudget();
+            foreach(Item Wank in items){
+                Console.WriteLine(Wank.ToString());
+            }
+            Item produce = new Item("Wetknees",100, 2, 4,true);
+            items.Add(produce);
+            InOut.saveBudget(items);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
