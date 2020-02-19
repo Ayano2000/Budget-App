c4d9d73d37146feeda97131bd2b67841979545a2
hbarnardWTC <wingarkorin@gmail.com>
2020-02-19 09:50:04 +0200

    Weee

--- budgetApp/Controllers/BudgetController.cs
+++ budgetApp/Controllers/BudgetController.cs
@@ -11,111 +11,75 @@ namespace budgetApp.Controllers
 {
     public class BudgetController : Controller
     {
-        private static Budget budget;
-
-        private static bool visited = false;
-
+        Budget budget = new Budget(InOut.readBudget());
         private readonly ILogger<BudgetController> _logger;
 
         public BudgetController(ILogger<BudgetController> logger)
         {
             _logger = logger;
         }
-
-        [HttpGet]
-        public IActionResult Budget() => View();
-
-        [HttpGet]
-        public IActionResult FixedBudget() => View();
-
-        [HttpGet]
-        public IActionResult GeneralBudget()
+        public IActionResult Budget(string mode = "new")
         {
-            if (visited == false)
+            if(mode.Equals("new"))
             {
-                string link = "http://localhost:5000/Budget/FixedBudget";
-                return Redirect(link);
+                System.IO.File.WriteAllText(@"UserData/budget.txt",string.Empty);
+                budget.budget = InOut.readBudget();
             }
-            return View();
-
+            return View(new LoadModel {Mode = mode, Budget = this.budget});
         }
 
-        [HttpGet]
-        public IActionResult VariableBudget()
+        public ActionResult NewItem()
         {
-            if (visited == false)
-            {
-                string link = "http://localhost:5000/Budget/FixedBudget";
-                return Redirect(link);
-            }
             return View();
         }
 
+        public ActionResult EditItem(string Name, string Amount, string Priority, string Rise, string Type)
+        {
+            return View(new DataViewModel {Name = Name, Amount = Amount, Priority = Priority, Rise = Rise, Expense = Type});
+        }
+
+        public ActionResult DeleteItem(string Name, string Amount, string Priority, string Rise, string Type)
+        {
+            budget.DeleteItem(Name);
+            InOut.saveBudget(budget.budget);
+            return View("Budget", new LoadModel {Mode = "load", Budget = this.budget});
+        }
 
         [HttpPost]
-        public ActionResult FixedBudget(string dummy) // dummy is necessary to prevent same params and return types on methods named the same
+        public ActionResult AddItem(string Name, double Amount, int Priority, int Rise, string Type)
         {
-            visited = true;
+            if(budget.ItemExists(Name))
+                return View("Budget", new LoadModel {Mode = "load", Budget = this.budget});
             try
             {
-                List<Item> FixedItems = new List<Item>();
-                FixedItems.Add(new Item("Salary", System.Convert.ToInt32(Request.Form["Salary"]), 5, 1, false));
-                FixedItems.Add(new Item("Other Income", System.Convert.ToInt32(Request.Form["Other"]), 5, 1, false));
-                FixedItems.Add(new Item("Rent", System.Convert.ToInt32(Request.Form["Rent"]), 5, 1, true));
-                FixedItems.Add(new Item("Insurance", System.Convert.ToInt32(Request.Form["Insurance"]), 5, 1, true));
-                FixedItems.Add(new Item("MedicalAid", System.Convert.ToInt32(Request.Form["MedicalAid"]), 5, 1, true));
-
-                budget = new Budget(FixedItems);
-                budget.PrintBudget();
-                return View();
+                Item item = new Item(Name,(int)Amount, Priority, Rise, Boolean.Parse(Type));
+                budget.AddItem(item);
+                InOut.saveBudget(budget.budget);
+                return View("Budget", new LoadModel {Mode = "load", Budget = this.budget});
             }
-            catch
+            catch(Exception e)
             {
-                return View();
+                Console.WriteLine(e);
+                return View("Budget", new LoadModel {Mode = "load", Budget = this.budget});
             }
         }
 
         [HttpPost]
-        public ActionResult GeneralBudget(string dummy) // dummy is necessary to prevent same params and return types on methods named the same
+        public ActionResult ChangeItem(string Name, double Amount, int Priority, int Rise, string Type)
         {
             try
             {
-                IList<Item> updatedBudget = budget.budget;
-                updatedBudget.Add(new Item("Groceries", System.Convert.ToInt32(Request.Form["Groceries"]), 5, 1, false));
-                updatedBudget.Add(new Item("Transport", System.Convert.ToInt32(Request.Form["Transport"]), 5, 1, false));
-                updatedBudget.Add(new Item("Utilities", System.Convert.ToInt32(Request.Form["Utilities"]), 5, 1, true));
-                updatedBudget.Add(new Item("Leisure", System.Convert.ToInt32(Request.Form["Leisure"]), 5, 1, true));
-                updatedBudget.Add(new Item("Phone", System.Convert.ToInt32(Request.Form["Phone"]), 5, 1, true));
-                updatedBudget.Add(new Item("Savings", System.Convert.ToInt32(Request.Form["Savings"]), 5, 1, true));
-                budget.budget = updatedBudget;
-                budget.PrintBudget();
-                return View();
+                budget.UpdateItemAmount(Name, (int)Amount);
+                budget.UpdateItemPriority(Name, Priority);
+                budget.UpdateItemRise(Name, Rise);
+                budget.UpdateItemType(Name, Boolean.Parse(Type));
+                InOut.saveBudget(budget.budget);
+                return View("Budget", new LoadModel {Mode = "load", Budget = this.budget});
             }
-            catch
+            catch(Exception e)
             {
-                return View();
-            }
-        }
-
-        [HttpPost]
-        public ActionResult VariableBudget(string dummy) // dummy is necessary to prevent same params and return types on methods named the same
-        {
-            try
-            {
-                Item to_add = new Item(Request.Form["Name"],
-                                        System.Convert.ToInt32(Request.Form["Amount"]),
-                                        System.Convert.ToInt32(Request.Form["Priority"]),
-                                        System.Convert.ToInt32(Request.Form["Rise"]),
-                                        System.Convert.ToBoolean(Request.Form["Expense"]));
-                IList<Item> VariableBudget = budget.budget;
-                VariableBudget.Add(to_add);
-                budget.budget = VariableBudget;
-                budget.PrintBudget();
-                return View();
-            }
-            catch
-            {
-                return View();
+                Console.WriteLine(e);
+                return View("Budget", new LoadModel {Mode = "load", Budget = this.budget});
             }
         }
 
